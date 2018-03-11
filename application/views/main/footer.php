1602d71069af9be68d0f87f79e2cc4594f4090be
Joseph Opanel <opanelj@gmail.com>
2018-03-10 19:15:32 -0800

    update

--- application/views/main/footer.php
+++ application/views/main/footer.php
@@ -5,7 +5,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <footer class="sticky-footer">
       <div class="container">
         <div class="text-center">
-          <small>© 2018 Lenapo Solutions LLC</small>
+          <small>© 2018 Lenapo Solutions LLC. DBA Insider Crypto</small>
         </div>
       </div>
     </footer>
