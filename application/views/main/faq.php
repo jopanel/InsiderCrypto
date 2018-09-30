bf289d985d913a881569574909bfe32cd5e7949f
jopanel <opanelj@yahoo.com>
2018-09-29 20:41:20 -0700

    update prices and working on faq

--- application/views/main/faq.php
+++ application/views/main/faq.php
@@ -10,12 +10,77 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         <li class="breadcrumb-item">
           Docs
         </li>
-        <li class="breadcrumb-item active">Blank Page</li>
+        <li class="breadcrumb-item active">FAQ</li>
       </ol>
       <div class="row">
+        <div class="col-12 text-center">
+          <h1>Frequently Asked Questions & Help</h1>
+          <h2>Definitions</h2>
+
+          <p><a href="#a1-what-is-arbitrage-in-trading">a1. What is arbitrage in trading?</a></p>
+          <p><a href="#a2-what-is-arbitrage-pair-events">a2. What is arbitrage pair events?</a></p>
+          <p><a href="#a3-what-is-an-exchange">a3. What is an exchange?</a></p>
+          <p><a href="#a4-what-does-the-threshold-mean">a4. What does the threshold mean?</a></p>
+          <p><a href="#a5-what-is-ai-and-what-does-it-do-here">a5. What is AI and what does it do here?</a></p>
+          <p><a href="#a6-what-is-a-symbol-or-market">a6. What is a symbol or market?</a></p>
+
+          <h2>Trading</h2>
+
+          <p><a href="#b1-is-there-instructions-on-how-to-trade-cryptocurrencies">b1. Is there instructions on how to trade cryptocurrencies?</a></p>
+          <p><a href="#b2-what-does-updated-mean-and-why-is-it-old">b2. What does updated mean and why is it old?</a></p>
+          <p><a href="#b3-what-is-the-number-next-to-the-opportunity">b3. What is the # number next to the opportunity?</a></p>
+          <p><a href="#b4-are-some-currencies-not-tradable">b4. Are some currencies not tradable?</a></p>
+          <p><a href="#b5-how-is-the-percentage-calculated">b5. How is the percentage calculated?</a></p>
+          <p><a href="#b6-do-you-have-access-to-my-cryptocurrency">b6. Do you have access to my cryptocurrency?</a></p>
+          <p><a href="#b7-does-insider-crypto-gaurantee-anything">b7. Does Insider Crypto gaurantee anything?</a></p>
+          <p><a href="#b8-what-risk-is-involved">b8. What risk is involved?</a></p>
+          <p><a href="#b9-some-of-my-matches-are-not-correct-prices-compared-to-the-exchange">b9. Some of my matches are not correct prices compared to the exchange.</a></p>
+          <p><a href="#b10-some-of-my-matches-are-not-correct-percentage">b10. Some of my matches are not correct percentage.</a></p>
+          <p><a href="#b11-some-of-my-matches-are-not-listed-correctly">b11. Some of my matches are not listed correctly.</a></p>
+
+          <h2>Billing</h2>
+
+          <p><a href="#c1-can-i-get-a-refund">c1. Can I get a refund?</a></p>
+          <p><a href="#c2-i-sent-too-much-when-making-a-payment-what-now">c2. I sent too much when making a payment, what now?</a></p>
+          <p><a href="#c3-i-didnt-send-enough-to-make-a-payment-and-now-i-must-pay-transfer-fees-again">c3. I didn't send enough to make a payment and now I must pay transfer fees again.</a></p>
+          <p><a href="#c4-i-made-my-payment-now-what">c4. I made my payment, now what?</a></p>
+          <p><a href="#c5-why-lisk">c5. Why Lisk?</a></p>
+          <p><a href="#c6-how-do-i-subscribe-to-monthly-payments">c6. How do I subscribe to monthly payments?</a></p>
+          <p><a href="#c7-do-you-accept-credit-cards-or-other-forms-of-payment">c7. Do you accept credit cards or other forms of payment?</a></p>
+          <p><a href="#c8-do-you-offer-a-free-trial">c8. Do you offer a free trial?</a></p>
+          <p><a href="#c9-can-i-purchase-packages-that-arent-listed">c9. Can I purchase packages that aren't listed?</a></p>
+
+          <h2>Preferences</h2>
+
+          <p><a href="#d1-how-do-i-add-my-exchanges">d1. How do I add my exchanges?</a></p>
+          <p><a href="#d2-what-is-the-threshold-percentage">d2. What is the threshold %?</a></p>
+          <p><a href="#d3-can-i-opt-in-or-out-of-email-notifications-for-opportunities-past-my-threshold-gain">d3. Can I opt in or out of email notifications for opportunities past my threshold gain?</a></p>
+          <p><a href="#d4-i-cant-find-my-exchange-listed-anymore-what-happened">d4. I can't find my exchange listed anymore what happened?</a></p>
+          <p><a href="#d5-i-added-my-exchanges-but-i-do-not-have-any-matches">d5. I added my exchanges but I do not have any matches?</a></p>
+          <p><a href="#d6-how-do-i-change-my-password">d6. How do I change my password?</a></p>
+          <p><a href="#d7-how-do-i-change-my-email-address">d7. How do I change my email address?</a></p>
+          <p><a href="#d8-why-do-you-keep-track-of-logged-ip-addresses">d8. Why do you keep track of logged IP addresses?</a></p>
+
+
+          <h2>Misc</h2>
+
+
+          <p><a href="#e-i-didnt-receive-my-email-verification">e1. I didn't receive my email verification?</a></p>
+          <p><a href="#e-i-registered-and-forgot-my-email-i-used">e2. I registered and forgot my email I used.</a></p>
+          <p><a href="#e-there-are-no-matches-available-right-now-or-the-site-is-not-working">e3. There are no matches available right now or the site is not working.</a></p>
+          <p><a href="#e-how-do-i-report-a-bug">e4. How do I report a bug?</a></p>
+          <p><a href="#e-i-see-too-many-connections-error">e5. I see "Too many connections" error.</a></p>
+          <p><a href="#e-i-found-a-website-error">e6. I found a website error.</a></p>
+          <p><a href="#e-the-website-is-running-slow">e7. The website is running slow.</a></p>
+        </div>
+      </div>
+
+      <div class="row">
         <div class="col-12">
-          <h1>Blank</h1>
-          <p>This is an example of a blank page that you can use as a starting point for creating new ones.</p>
+          <div id="#a1-what-is-arbitrage-in-trading"><p><b>What is arbitrage in trading?</b> Arbitrage trading is the action of buying a currency, stock, or other commoditiy and transferring it to another exchange/market to sell for a profit. Every exchange has its own market. Not every holder of the commodity is apart of the exchange. Therefore occasionally the price may fluctuate strongly between two markets. In this case investors have an opporunity to make a profit transferring to the exchange that is selling at a higher price.</p>
+          </div>
+
+          <div id="a2-what-is-arbitrage-pair-events"><p><b>What is arbitrage pair events?</b>
         </div>
       </div>
     </div>
